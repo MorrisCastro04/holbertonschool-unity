d5b960c751808f9e0a20276fda0c301d8f030acb
Morris Jomar <jomarcastro2004@gmail.com>
2024-10-17 23:15:24 -0400

    invert Toggle Y

--- unity-assets_ui/Assets/Scripts/OptionsMenu.cs
+++ unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    public Toggle invertToggleY;
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("InvertY"))
+        {
+            invertToggleY.isOn = PlayerPrefs.GetInt("InvertY") == 1;
+        }
+        else
+        {
+            invertToggleY.isOn = false;
+        }
+        invertToggleY.onValueChanged.AddListener(delegate { ToggleInvertY(invertToggleY.isOn); });
+    }
     public void Back()
     {
         SceneManager.LoadScene(0);
+    }
+    void ToggleInvertY(bool invertY)
+    {
+        PlayerPrefs.SetInt("InvertY", invertY ? 1 : 0);
+        PlayerPrefs.Save();
     }
 }
