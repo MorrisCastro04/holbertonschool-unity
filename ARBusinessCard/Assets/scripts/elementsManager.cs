b4327ed557df1b7e6f1660f8e4afdc8810a97c1f
Morris Jomar <jomarcastro2004@gmail.com>
2024-12-05 22:09:06 -0400

    change sprites to ui images

---
+++ ARBusinessCard/Assets/scripts/elementsManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class elementsManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void google()
+    {
+        Application.OpenURL("https://www.google.com");
+    }
+}
